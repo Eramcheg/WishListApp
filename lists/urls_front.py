21ab8c2c76b1d1d977ce15024eccb20d5749c6ef
Eramcheg <eramcheg@gmail.com>
2025-10-08 00:02:09 +0200

    Update: * Add cancel and edit operations to wishlists and items

--- lists/urls_front.py
+++ lists/urls_front.py
@@ -2,9 +2,13 @@ from django.urls import path
 
 from .views_front import (
     ItemCreateView,
+    ItemDeleteView,
+    ItemUpdateView,
     WishlistCreateView,
+    WishlistDeleteView,
     WishlistDetailView,
     WishlistListView,
+    WishlistUpdateView,
 )
 
 urlpatterns = [
@@ -13,4 +17,8 @@ urlpatterns = [
     path("<int:pk>/new/", ItemCreateView.as_view(), name="item_create"),
     path("<int:pk>/", WishlistDetailView.as_view(), name="wishlist_detail"),
     path("new/", WishlistCreateView.as_view(), name="wishlist_create"),
+    path("<int:pk>/edit/", WishlistUpdateView.as_view(), name="wishlist_edit"),
+    path("<int:pk>/delete/", WishlistDeleteView.as_view(), name="wishlist_delete"),
+    path("item/<int:pk>/edit/", ItemUpdateView.as_view(), name="item_edit"),
+    path("item/<int:pk>/delete/", ItemDeleteView.as_view(), name="item_delete"),
 ]
--- lists/views_front.py
+++ lists/views_front.py
@@ -1,8 +1,15 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.urls import reverse, reverse_lazy
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
 
 from .forms import ItemForm, WishlistForm
 from .models import Item, Wishlist
@@ -15,6 +22,42 @@ class WishlistListView(ListView):
 
     def get_queryset(self):
         return Wishlist.objects.filter(owner=self.request.user)
+
+
+@method_decorator(login_required, name="dispatch")
+class WishlistUpdateView(UpdateView):
+    model = Wishlist
+    form_class = WishlistForm
+    template_name = "lists/wishlist_form.html"  # тот же шаблон, что и для create
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(owner=self.request.user)
+
+    def get_success_url(self):
+        messages.success(self.request, "Wishlist updated")
+        return reverse("wishlist_detail", kwargs={"pk": self.object.pk})
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if isinstance(self.object, Item):
+            ctx["cancel_url"] = reverse("wishlist_detail", kwargs={"pk": self.object.wishlist_id})
+        else:
+            ctx["cancel_url"] = reverse("wishlist_detail", kwargs={"pk": self.object.pk})
+        return ctx
+
+
+@method_decorator(login_required, name="dispatch")
+class WishlistDeleteView(DeleteView):
+    model = Wishlist
+    template_name = "lists/confirm_delete.html"
+    success_url = reverse_lazy("wishlist_list")
+
+    def get_queryset(self):
+        return Wishlist.objects.filter(owner=self.request.user)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "Wishlist deleted")
+        return super().delete(request, *args, **kwargs)
 
 
 @method_decorator(login_required, name="dispatch")
@@ -34,6 +77,14 @@ class WishlistCreateView(CreateView):
         form.instance.owner = self.request.user
         return super().form_valid(form)
 
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if isinstance(self.object, Item):
+            ctx["cancel_url"] = reverse("wishlist_list")
+        else:
+            ctx["cancel_url"] = reverse("wishlist_list")
+        return ctx
+
 
 @method_decorator(login_required, name="dispatch")
 class ItemCreateView(CreateView):
@@ -47,11 +98,50 @@ class ItemCreateView(CreateView):
     def get_context_data(self, **kwargs):
         ctx = super().get_context_data(**kwargs)
         ctx["wishlist"] = self.get_wishlist()
+        if isinstance(self.object, Item):
+            ctx["cancel_url"] = reverse("wishlist_list")
+        else:
+            ctx["cancel_url"] = reverse("wishlist_list")
         return ctx
 
     def form_valid(self, form):
-        form.instance.wishlist = self.get_wishlist()  # <<< привязка к списку
+        form.instance.wishlist = self.get_wishlist()
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse("wishlist_detail", kwargs={"pk": self.kwargs["pk"]})
+
+
+@method_decorator(login_required, name="dispatch")
+class ItemUpdateView(UpdateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "lists/wishlist_item_form.html"
+
+    def get_queryset(self):
+        return Item.objects.filter(wishlist__owner=self.request.user)
+
+    def get_success_url(self):
+        messages.success(self.request, "Item updated")
+        return reverse("wishlist_detail", kwargs={"pk": self.object.wishlist_id})
+
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        if isinstance(self.object, Item):
+            ctx["cancel_url"] = reverse("wishlist_detail", kwargs={"pk": self.object.wishlist_id})
+        else:
+            ctx["cancel_url"] = reverse("wishlist_detail", kwargs={"pk": self.object.pk})
+        return ctx
+
+
+@method_decorator(login_required, name="dispatch")
+class ItemDeleteView(DeleteView):
+    model = Item
+    template_name = "lists/confirm_delete.html"
+
+    def get_queryset(self):
+        return Item.objects.filter(wishlist__owner=self.request.user)
+
+    def get_success_url(self):
+        messages.success(self.request, "Item deleted")
+        return reverse("wishlist_detail", kwargs={"pk": self.object.wishlist_id})
